7db5055a92d75c400cd803a9b4e67fc1fb67a837
greguz <gregolettogiacomo@gmail.com>
2019-08-30 10:08:19 +0200

    Improve socket logging

--- src/socket/index.js
+++ src/socket/index.js
@@ -61,43 +61,94 @@ async function messageHandler(socket, data) {
   await command.handler.call(this, socket, packet)
 }
 
+function startTimer() {
+  const start = new Date()
+  return function endTimer() {
+    const end = new Date()
+    return end.getTime() - start.getTime()
+  }
+}
+
 function plugin(fastify, options, callback) {
+  let connectionsCounter = 1
   let interval
 
+  fastify.route({
+    method: 'GET',
+    url: '/api/v1/test',
+    handler(request, reply) {
+      reply.status(204).send()
+    }
+  })
+
+  // Clean up procedure
   fastify.addHook('onClose', (f, done) => {
     clearInterval(interval)
     done()
   })
 
   fastify.ready(err => {
-    if (!err) {
-      const server = fastify.ws
-
-      interval = setInterval(() => {
-        for (const client of server.clients) {
-          if (!client.isAlive) {
-            console.log('Offline')
-            client.terminate()
-          } else {
-            client.isAlive = false
-            client.ping(_.noop)
-          }
-        }
-      }, 5000) // TODO from options
-
-      server.on('connection', client => {
-        client.on('error', err => fastify.log.error(err))
-
-        client.isAlive = true
-        client.on('pong', () => (client.isAlive = true))
-
-        client.on('message', data => {
-          messageHandler
-            .call(fastify, client, data)
-            .catch(err => fastify.log.error(err))
-        })
-      })
+    if (err) {
+      return
     }
+
+    // WebSocket server instance (from another plugin)
+    const server = fastify.ws
+
+    // Detect disconnected sockets
+    interval = setInterval(() => {
+      for (const client of server.clients) {
+        if (!client.isAlive) {
+          client.log.info('websocket disconnected')
+          client.terminate()
+        } else {
+          client.isAlive = false
+          client.ping(_.noop)
+        }
+      }
+    }, 5000)
+
+    server.on('connection', (client, request) => {
+      let messagesCounter = 1
+
+      // Setup custom logger
+      client.log = fastify.log.child({ socketId: connectionsCounter++ })
+
+      // Notify connection
+      client.log.info({
+        request: {
+          method: request.method,
+          url: request.url
+          // TODO: hostname
+          // TODO: remoteAddress
+          // TODO: remotePort
+        },
+        msg: 'websocket connected'
+      })
+
+      // Handle client errors
+      client.on('error', err => client.log.error(err))
+
+      // Start keep alive communication
+      client.isAlive = true
+      client.on('pong', () => (client.isAlive = true))
+
+      // Handle messages
+      client.on('message', data => {
+        const timer = startTimer()
+        const logger = client.log.child({ messageId: messagesCounter++ })
+
+        messageHandler
+          .call(fastify, client, data)
+          .catch(err => logger.error(err))
+          .then(() =>
+            logger.info({
+              handleTime: timer(),
+              msg: 'message handled'
+            })
+          )
+      })
+    })
   })
 
   callback()
